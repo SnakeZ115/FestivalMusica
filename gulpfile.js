a99bddb29920be2af720b223bf0b857eaf98c959
SnakeZ115 <joaorsa.ico@gmail.com>
2025-07-29 17:22:54 -0700

    SCSS añadido y Header listo

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+import { src, dest, watch } from 'gulp'
+
+const sass = gulpSass(dartSass); // compilar sass con gulpsass
+
+export function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css') )
+    done()
+}
+
+export function dev() {
+    watch('src/scss/**/*.scss', css) // habilitar modo watch para reflejar cambios inmediatamente. el ** indica que busque todos los archivos con extencion .scss
+}
+
+/*
+// se crean funciones
+// export te da la opcion de ejecutar la funcion en package.json
+export function hola( done ) {
+    console.log("hola desde gulp")
+    done() // finaliza la funcion
+}
+*/
